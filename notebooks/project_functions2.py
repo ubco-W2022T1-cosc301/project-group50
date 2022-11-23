fa70bceea6d1e01679fa135639bf95c2ba8ed35d
Justindilabio <jdilabio123@gmail.com>
2022-11-23 11:58:32 -0800

    justin dashboard

--- notebooks/project_functions2.py
+++ notebooks/project_functions2.py
@@ -4,7 +4,7 @@ def pipeline1(path):
     HD = pd.read_csv(path)
 
     df = (
-    HD.loc[:,['HighBP','BMI','HighChol','Diabetes','HeartDiseaseorAttack','Stroke']]
-    .loc[(HD["BMI"] <60)]
+        HD.loc[:,['HighBP','BMI','HighChol','Diabetes','HeartDiseaseorAttack','Stroke']]
+        .loc[(HD["BMI"] <60)]
     )
     return df
